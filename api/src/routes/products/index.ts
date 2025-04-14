95095f502a62b93e7f29294a3a91f8b78f690682
ishaan <ishaanbhalla1998@gmail.com>
2025-04-14 15:26:47 -0700

    feat: add products controller

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,19 +1,23 @@
 import { Router } from "express";
+import {
+  listProducts,
+  getProductbyId,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./productsController";
 
 // Products endpoints
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("The list of products");
-});
+router.get("/", listProducts);
 
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("Product B");
-});
+router.get("/:id", getProductbyId);
 
-router.post("/", (req, res) => {
-  res.send("Product created");
-});
+router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
